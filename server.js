751070283e279f8d2354ae68f06adbcc0b7dc7f0
Alana Franklin <alanafrankly@gmail.com>
2017-07-31 15:38:52 -0700

    Started working user post request, but hit bug

--- server.js
+++ server.js
@@ -4,10 +4,28 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const pg = require('pg');
-const connString = 'postgress://localhost:5432/yourtube';
+const connString = 'postgres://localhost:5432/yourtube';
+// const connString = process.env.PG_PASSWORD;
+const client = new pg.Client(connString);
+client.connect();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
+
+app.post('/newUser', function(request, response) {
+  console.log(request.name);
+  client.query(`INSERT INTO users (name) VALUES ($1);`, [request.name]),
+  function(err) {
+    if (err) console.error(err)
+  };
+});
+
+loadDB();
+function loadDB(){
+  client.query(`CREATE TABLE IF NOT EXISTS users
+    (user_id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, preferences VARCHAR);`);
+}
+
 
 app.listen(PORT, function(){
   console.info('Listening on port: ' + PORT);
