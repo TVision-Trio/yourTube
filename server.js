e5f1c1f290fa80951c137ecd3c8d902d5bb9e98d
Aeone Singson <aeone.singson@gmail.com>
2017-08-01 12:16:48 -0700

    created data tables

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const pg = require('pg');
 const PORT = process.env.PORT || 3000;
-const connString = 'postgres://localhost:5432/yourtube';
-// const connString = process.env.PG_PASSWORD;
+// const connString = 'postgres://localhost:5432/yourtube';
+const connString = process.env.PG_PASSWORD;
 const client = new pg.Client(connString);
 client.connect();
 
@@ -37,8 +37,8 @@ app.put('/updateUser', function (req, res) {
     res.send(result.rows[0]);
   })
   .catch(function (err) {
-    console.log(req.body);
-    console.error(err);
+    console.log(req.body)
+    console.error(err)
     res.send(err);
   });
 });
@@ -49,20 +49,80 @@ app.get('/getUser', function (req, res) {
     `SELECT * FROM users WHERE user_id=$1`, [req.query.user_id]
   )
   .then(function (result) {
-    console.log('Inside server GET success: ');
-    console.log(result);
+    console.log('Inside server GET success: ')
+    console.log(result)
     res.send(result.rows[0]);
   })
   .catch(function (err) {
-    console.log('Inside server GET error: ');
+    console.log('Inside server GET error: ')
     console.error(err);
     res.send(err);
   });
 });
 
 loadDB();
+
 function loadDB(){
-  client.query(`CREATE TABLE IF NOT EXISTS users (user_id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, genres VARCHAR ARRAY, days VARCHAR ARRAY, times VARCHAR ARRAY );`)
+  let dayArray = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+  let timeArray = ['morning', 'afternoon', 'evening'];
+  // create main table
+  client.query(`CREATE TABLE IF NOT EXISTS users (user_id SERIAL PRIMARY KEY, name VARCHAR NOT NULL);`)
+  createGenreTable();
+  createDaysTable();
+  createTimesTable();
+
+  // create tables for genre + days + times static data
+  function createGenreTable() {
+    client.query(`CREATE TABLE IF NOT EXISTS genres (
+      genre_id SERIAL PRIMARY KEY,
+      genre VARCHAR
+    );`
+  ).then(function (){
+    console.info('Created genres table');
+  });
+  }
+
+  // create days table
+  function createDaysTable() {
+    client.query(`CREATE TABLE IF NOT EXISTS days (
+      day_id SERIAL PRIMARY KEY,
+      day VARCHAR
+    );`
+  ).then(function (){
+    console.info('Created days table');
+    dayArray.forEach(function(day){insertDaysData(day);});
+  });
+  }
+
+  function insertDaysData(thisDay) {
+    console.log('this has been called');
+    client.query(`INSERT INTO days (day) VALUES ($1) ON CONFLICT DO NOTHING RETURNING *;`,
+      [ thisDay ]).then(function(){
+        console.log(thisDay);
+        console.info('Insert days into table');
+      });
+  }
+
+  // create times table
+  function createTimesTable() {
+    client.query(`CREATE TABLE IF NOT EXISTS times (
+      time_id SERIAL PRIMARY KEY,
+      time VARCHAR
+    );`
+  ).then(function (){
+    console.info('Created times table');
+    timeArray.forEach(function(time){insertTimesData(time);});
+  });
+  }
+
+  function insertTimesData(thisTime) {
+    console.log('this has been called');
+    client.query(`INSERT INTO times (time) VALUES ($1) ON CONFLICT DO NOTHING RETURNING *;`,
+      [ thisTime ]).then(function(){
+        console.log(thisTime);
+        console.info('Insert times into table');
+      });
+  }
 }
 
 app.listen(PORT, function(){
