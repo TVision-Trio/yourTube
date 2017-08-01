c047dd1dafa6e0a40c2ce6fe1dc2a6e4aaf2a28a
Alana Franklin <alanafrankly@gmail.com>
2017-07-31 22:46:59 -0700

    Finished user.getUser function to find get data from database based on user id

--- public/scripts/model/userModel.js
+++ public/scripts/model/userModel.js
@@ -1,27 +1,33 @@
 'use strict';
 
 var app = app || {};
-var testUser = {name: 'Alana', user_id: 66, preferences: {
-  genres: ['comedy', 'horror'],
-  days: ['monday', 'tuesday', 'friday'],
-  times: ['evening']
-}};
+var testUser = {name: 'Alana', user_id: 1, genres: ['comedy', 'horror'], days: ['monday', 'tuesday', 'friday'], times: ['evening']
+};
 
 (function(module){
 
   function User(userData){
     this.name = userData.name;
-    this.preferences = userData.pref;
     if (userData.user_id) {
       this.user_id = userData.user_id;
     }
+    if (userData.genres) {
+      this.genres = userData.genres;
+    }
+    if (userData.days) {
+      this.days = userData.days;
+    }
+    if (userData.times) {
+      this.times = userData.times;
+    }
   }
 
-  var test = new User(testUser);
+  var currentUser = new User(testUser);
 
   // CREATE user in database based on user input
+  // TODO: handle user input error
   User.prototype.createUser = function() {
-    $.post('/newUser', {name: this.name, preferences: this.preferences}).then(function(results){
+    $.post('/newUser', {name: this.name}).then(function(results){
       console.log(results);
       this.user_id = results.user_id;
     }, function(error){
@@ -29,7 +35,7 @@ var testUser = {name: 'Alana', user_id: 66, preferences: {
     });
   };
 
-  // TODO: UPDATE user function
+  // UPDATE user in database based on updated preferences
   // TODO: Would this ever be called on a user that has not been created in the database? If so, this function will throw an error because the user.id will not yet exisit
   User.prototype.updateUser = function() {
     if (!this.user_id){
@@ -38,7 +44,7 @@ var testUser = {name: 'Alana', user_id: 66, preferences: {
       $.ajax({
         url:'/updateUser',
         method: 'PUT',
-        data: {name: this.name, user_id: this.user_id, preferences: this.preferences}
+        data: {name: this.name, user_id: this.user_id, genres: this.genres, days: this.days, times: this.times}
       }).then(function(results){
         console.log(results);
       }, function(error){
@@ -46,10 +52,22 @@ var testUser = {name: 'Alana', user_id: 66, preferences: {
       });
     };
   };
-  test.updateUser();
 
-  // TODO: handle user input error
+  // TODO: GET data from database by user_id
+  User.prototype.getUser = function() {
+    $.ajax({
+      url:'/getUser',
+      method: 'GET',
+      data: {user_id: this.user_id}
+    }).then(function(results){
+      console.log(results);
+    }, function(error){
+      console.error(error);
+    });
+  };
 
-  // TODO: global user object
+  // TODO: How does this currentUser variable get updated?
+  // Save current user to global app
+  module.currentUser = currentUser;
 
 })(app);
--- server.js
+++ server.js
@@ -25,31 +25,45 @@ app.post('/newUser', function (req, res) {
   .catch(function (err) {
     console.error(err);
     res.send(err);
-  })
-})
+  });
+});
 
 app.put('/updateUser', function (req, res) {
   client.query(
-    `UPDATE users SET name=$1 WHERE user_id=$2 RETURNING *`,
-    [req.body.name, req.body.user_id]
+    `UPDATE users SET name=$1, genres=$2, days=$3, times=$4 WHERE user_id=$5 RETURNING *`,
+    [req.body.name, req.body.genres, req.body.days, req.body.times, req.body.user_id]
   )
   .then(function (result) {
-    console.log('Inside server put success: ');
+    res.send(result.rows[0]);
+  })
+  .catch(function (err) {
+    console.log(req.body);
+    console.error(err);
+    res.send(err);
+  });
+});
+
+app.get('/getUser', function (req, res) {
+  console.log(req.query.user_id);
+  client.query(
+    `SELECT * FROM users WHERE user_id=$1`, [req.query.user_id]
+  )
+  .then(function (result) {
+    console.log('Inside server GET success: ');
     console.log(result);
     res.send(result.rows[0]);
   })
   .catch(function (err) {
-    console.log('Inside server put error: ');
+    console.log('Inside server GET error: ');
     console.error(err);
     res.send(err);
-  })
-})
+  });
+});
 
 loadDB();
 function loadDB(){
-  client.query(`CREATE TABLE IF NOT EXISTS users (user_id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, preferences VARCHAR);`)
+  client.query(`CREATE TABLE IF NOT EXISTS users (user_id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, genres VARCHAR ARRAY, days VARCHAR ARRAY, times VARCHAR ARRAY );`)
 }
-
 
 app.listen(PORT, function(){
   console.info('Listening on port: ' + PORT);
