62b7ca518afbf1df2413762437693d1b1b40d94a
jp <gaines.johnp@gmail.com>
2017-08-02 12:41:59 -0700

    added mock data

---
+++ public/scripts/controller/userController.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//add handler for landing page
+var app = app ||{};
+
+(function(){
+  var userController = {};
+
+  userController.index =function(){
+    $ajax({
+      url:'/getUsers',
+      method: GET,
+    }).then(function(results){
+      console.log(results);
+    }), function(error){
+      console.log('error');      
+    }
+  }
+
+  // $('#input').on('submit', (e) => {   
+  //  e.preventDefault();     
+  // })
+  //  console.log(newUser);
+
+
+})(app)
