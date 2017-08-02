cdaee9cfa480cb5f095f0985971452e6c5823800
Alana Franklin <alanafrankly@gmail.com>
2017-08-02 13:06:04 -0700

    Started working on cloud styles

--- public/scripts/controller/userController.js
+++ public/scripts/controller/userController.js
@@ -6,19 +6,19 @@ var app = app ||{};
 (function(){
   var userController = {};
 
-  userController.index =function(){
+  userController.index = function(){
     $ajax({
       url:'/getUsers',
       method: GET,
     }).then(function(results){
       console.log(results);
     }), function(error){
-      console.log('error');      
+      console.log('error');
     }
   }
 
-  // $('#input').on('submit', (e) => {   
-  //  e.preventDefault();     
+  // $('#input').on('submit', (e) => {
+  //  e.preventDefault();
   // })
   //  console.log(newUser);
 
