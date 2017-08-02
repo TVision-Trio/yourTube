147cc79e32a6ddc13c6ef7cbcbbe359b8e0e7b02
Alana Franklin <alanafrankly@gmail.com>
2017-08-02 13:54:30 -0700

    Fixed display shows on button click (fake preferences)

--- public/scripts/controller/showController.js
+++ public/scripts/controller/showController.js
@@ -20,12 +20,13 @@ var app = app || {};
   };
 
   // On Submit, query API and get filtered show list based on preferences
-  $('#querySubmitButton').on(function(){
-    module.DataModel.requestShows( (filteredShows) => {
-      filteredShows = module.DataModel.filterShows(['Comedy','Drama'],['Monday', 'Tuesday'], ['Evening']);
+  $('#querySubmitButton').on('click', function(){
+    module.DataModel.requestShows( (mappedData) => {
+      console.log(mappedData);
+      var filteredShows = module.DataModel.filterShows(['Comedy','Drama'],['Monday', 'Tuesday'], ['Evening']);
+      console.log(filteredShows);
       module.populateResults(filteredShows);
     })
-    // TODO: Pass filtered shows to view function to display
   });
 
 
