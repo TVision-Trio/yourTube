6c083ad4394f34c06c7e42c7d4956b8015b31ca5
Aeone Singson <aeone.singson@gmail.com>
2017-08-02 11:06:49 -0700

    got all genres from API. printing to console

--- public/scripts/controller/showController.js
+++ public/scripts/controller/showController.js
@@ -19,15 +19,13 @@ var app = app || {};
     module.populateTimes(times);
   };
 
-
   // On Submit, query API and get filtered show list based on preferences
   $('#querySubmitButton').on(function(){
-  });
-  module.DataModel.requestShows(function(){
     var filteredShows = module.DataModel.filterShows(['Comedy','Drama'],['Monday', 'Tuesday'], ['Evening']);
     // TODO: Pass filtered shows to view function to display
     module.populateResults(filteredShows);
   });
+
 
   // On 'detail' click, pass show id to home view to display.
   $('.show-more').on(function(){
